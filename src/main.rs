3b0b8f83924777407f787a2119a6b35f2741a628
Alexis Simon <alexis.simon@normalesup.org>
2023-02-26 21:57:21 -0800

    try infinite loop to restart

--- src/main.rs
+++ src/main.rs
@@ -12,26 +12,33 @@ use futures_util::TryStreamExt;
 async fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
     let mastodata: &String = &args[1];
-    run(&mastodata).await?;
 
+    let mut count = 0u32;
+    loop {
+        run(mastodata).await?;
+        count += 1;
+        if count == 10 {
+            println!("Retried {count} times, I had enough!");
+            break;
+        }
+    }
     Ok(())
 }
 
 async fn run(mastodata: &String) -> Result<()> {
-    let mastodon = if let Ok(data) = toml::from_file(&mastodata) {
+    let mastodon = if let Ok(data) = toml::from_file(mastodata) {
         Mastodon::from(data)
     } else {
-        register(&mastodata).await?
+        register(mastodata).await?
     };
-    let stream = mastodon.stream_user().await?;
+    let stream = mastodon.stream_notifications().await?;
     println!(
-        "watching mastodon for events. This will run forever, press Ctrl+C to kill the program."
+        "watching mastodon for notifications. This will run forever, press Ctrl+C to kill the program."
     );
     stream
         .try_for_each(|(event, mastodon)| async move {
-            match event {
-                Event::Notification(notif) => notif_handler(notif, mastodon).await?,
-                _ => (),
+            if let Event::Notification(notif) = event {
+                notif_handler(notif, mastodon).await?;
             }
             Ok(())
         })
